effaf5fbc27ddb59aa0b64bba00de5a26f39d92d
John Belgira <belgira.jna@gmail.com>
2018-06-04 07:41:39 -0400

    adding files

---
+++ datastore.js
@@ -0,0 +1,91 @@
+// JS to store all data: questions, answers in an array, correct answer at the index of the array, pictures & alts
+
+const quizStore = [
+	{ 
+		questionText: 'Who was the first avenger recruited by Nick Fury?',
+		answers: ['Bruce Banner','Thor Odinson','Steve Rogers','Tony Stark'],
+		correctAnswer: 3,
+		answerImage: 'http://oyster.ignimgs.com/mediawiki/apis.ign.com/marvel-studios-cinematic-universe/3/32/Nfury2.jpg',
+		answerImageAlt: 'Tony Stark talking with Nick Fury'
+	},
+
+	{
+		questionText: 'In The Avengers, what was the alien race that invaded and attacked New York City?',
+		answers: ['Eridani','Kree','Chitauri','Wraiths'],
+		correctAnswer: 2,
+		answerImage: 'http://24.media.tumblr.com/tumblr_mavnvoVW7L1rxtgtno2_1280.jpg',
+		answerImageAlt: 'Chitarui alien species attacking NYC'
+
+	},
+
+	{
+		questionText: 'First appearing in 2013, which infinity stone is the color red?',
+		answers: ['Heart Stone','Power Stone','Soul Stone','Reality Stone'],
+		correctAnswer: 3,
+		answerImage: 'https://media.eventhubs.com/images/2017/09/25-infinity-stones-marvel-vs-capcom-infinite-reality-looking-most-c_fgMXM6k.jpg',
+		answerImageAlt: 'Red infinity stone'
+	},
+
+	{
+		questionText: 'In Avengers: Age of Ultron, a city was completely destroyed in which fictional country?',
+		answers: ['Andorsa','Sokovia','Sakar','Moldovia'],
+		correctAnswer: 1,
+		answerImage: 'https://media1.giphy.com/media/8Uyt6HvYlQwkU/giphy.gif',
+		answerImageAlt: 'Country of Sokovia being destroyed'
+	},
+
+	{
+
+		questionText: 'Which film begins Phase 2 of the MCU?',
+		answers: ['Iron Man 3', 'Thor: Dark World', 'Ant-Man', 'Guardians of the Galaxy Vol. 2'],
+		correctAnswer: 0,
+		answerImage: 'https://www.sciencebuddies.org/Files/4612/11/2013-ironman_wallpaper_robert_downey_IronMan3_wp_7_w_820px.png',
+		answerImageAlt: 'Iron Man 3 movie poster'
+	},
+
+	{
+
+		questionText: 'Which infinity stone was introduced last?',
+		answers: ['Space Stone', 'Time Stone', 'Mind Stone', 'Soul Stone'],
+		correctAnswer: 3,
+		answerImage: 'https://www.quirkybyte.com/wp-content/uploads/2018/03/thanos-780x405-1.jpg',
+		answerImageAlt: "Orange infinity stone on Thanos's gauntlet"
+	},
+
+	{
+		questionText: 'What is the name of the henchmen group that directly serves Thanos?',
+		answers: ['Acolytes', 'Black Order', 'Dark Enforcers', 'Great Emissaries'],
+		correctAnswer: 1,
+		answerImage: 'https://i1.wp.com/bestofcomicbooks.com/wp-content/uploads/2018/03/29250015_926799224158394_2267228985479921664_o.jpg?fit=1200%2C630',
+		answerImageAlt: 'Four members of the Black Order'
+	},
+
+	{
+		questionText: 'As of May 2018, how many movies comprise the MCU?',
+		answers: ['Twenty-One', 'Nineteen', 'Sixteen', 'Seventeen'],
+		correctAnswer: 1,
+		answerImage: 'https://www.screengeek.net/wp-content/uploads/2017/09/marvel-cinematic-universe-easter-eggs.jpg',
+		answerImageAlt: 'All of the avengers to date assembled'
+
+	},
+
+	{
+		questionText: "!SPOILER ALERT!<br> In Avengers: Infinity War, how many onscreen characters DO NOT survive when Thanos snaps his fingers?",
+		answers: ['Eight', 'Nine', 'Ten', 'Thirteen'],
+		correctAnswer: 2,
+		answerImage: 'https://memegenerator.net/img/images/14279974/pile-of-ashes.jpg',
+		answerImageAlt: 'A pile of dust'
+	},
+		
+	{
+		questionText: "!BONUS!<br> Which country is Wakanda closest to?",
+		answers: ['Kenya', 'Senegal', 'Tanzania', 'Gabon'],
+		correctAnswer: 0,
+		answerImage: 'https://qph.fs.quoracdn.net/main-qimg-5247f022dd80d75ff4030f7f48792cbd',
+		answerImageAlt: 'Geographic location of Wakanda on map '
+	},
+];
+
+
+
+
